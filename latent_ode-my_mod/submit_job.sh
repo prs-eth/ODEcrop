a7494958d96c1b20e50b72301d015906e485b5fe
Nando Metzger <metzgern@lo-login-01.leonhard.ethz.ch>
2020-03-12 11:31:35 +0100

    update leonhard-job-file

---
+++ latent_ode-my_mod/submit_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BSUB -W 24:00
+##BSUB -o /cluster/work/igp_psr/metzgern/Network/outputs/outputtrain_Le${object}.%J.%I.txt
+##BSUB -e /cluster/work/igp_psr/metzgern/Network/outputs/train_Le${object}.%J.%I.txt
+#BSUB -R "rusage[mem=32000,ngpus_excl_p=1]"
+#BSUB -n 1
+##BSUB -Is 
+#BSUB -N
+#BSUB -B
+#### BEGIN #####
+
+
+module load python_gpu/3.6.1 cudnn/7.5 cuda/10.0.130 hdf5/1.10.1 #2.7.14 #numpy/1.16.3 matplotlib/2.1.1 Keras/2.2.4
+
+#sh test_cityscapes.sh
+python AE_L1_leon_withskip.py
+
