d5f127d844fa3c29c2797d193471e5bee5692140
nandometzger <nando.metzger1996@gmail.com>
2020-03-12 08:30:38 +0100

    another commit

--- latent_ode-my_mod/run_crop.sh
+++ latent_ode-my_mod/run_crop.sh
@@ -1,2 +1,3 @@
-python3 run_models.py --niters 3 -n 300000 -b 500 -l 15 --dataset crop --ode-rnn --rec-dims 100 --rec-layers 4 --gen-layers 1 --units 500 --gru-units 50 --classif --ode-method dopri5
+python3 run_models.py --niters 3 -n 300000 -b 2000 -l 15 --dataset crop --ode-rnn --rec-dims 100 --rec-layers 4 --gen-layers 1 --units 500 --gru-units 50 --classif --ode-method euler
+
 
